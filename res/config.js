c5549587175f8d4ad2c60280e5b9393c11149d45
Fr0xty <86002969+Fr0xty@users.noreply.github.com>
2022-01-31 13:01:56 +0800

    Update config.js

--- res/config.js
+++ res/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    token: 'ODgyODQwODYzMTU0MjcwMjg5.YTBPoA.VvbKthaxB7uUwk0NOMXI8GqhNtU',
-    // token: process.env.TOKEN,
+    token: process.env.TOKEN,
     prefix: 'test ',
     hinaColor: '#E49CFF'
 };
