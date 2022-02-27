e2a11a3becea34bbd4aa66542b931b2accec6337
Fr0xty <86002969+Fr0xty@users.noreply.github.com>
2022-02-27 06:55:20 +0000

    updated waifu.im API integration

--- commands/image/waifu.js
+++ commands/image/waifu.js
@@ -8,7 +8,7 @@ import { prefix, hinaColor, hinaImageOption } from '../../res/config.js';
 export default {
 
     name: 'waifu',
-    aliases: ['maid', 'ass', 'ecchi', 'ero', 'hentai', 'nsfwmaid', 'milf', 'oppai', 'oral', 'paizuri', 'selfies', 'uniform', 'random'],
+    aliases: ['uniform', 'maid', 'marin-kitagawa', 'mori-calliope', 'raiden-shogun', 'selfies', 'random', 'ass', 'ecchi', 'ero', 'hentai', 'milf', 'oral', 'paizuri', 'breast', 'nsfwmaid', 'nsfwuniform', 'nsfwwaifu', 'nsfwselfies', 'nsfwrandom'],
     description: 'get nice pictures.',
 
 
@@ -25,53 +25,77 @@ export default {
 
         // tag
         const tag = msg.content.slice(prefix.length).split(' ').shift().toLowerCase();
-        let endpoint;
+        let url;
         switch (tag) {
-            case ('waifu'):
-                endpoint = 'sfw/waifu';
+            // sfw
+            case ('uniform'):
+                url = 'https://api.waifu.im/random/?selected_tags=uniform&is_nsfw=false&many=true';
                 break;
             case ('maid'):
-                endpoint = 'sfw/maid';
+                url = 'https://api.waifu.im/random/?selected_tags=maid&is_nsfw=false&many=true';
                 break;
-            case ('ass'):
-                endpoint = 'nsfw/ass';
+            case ('waifu'):
+                url = 'https://api.waifu.im/random/?selected_tags=waifu&is_nsfw=false&many=true';
                 break;
-            case ('ecchi'):
-                endpoint = 'nsfw/ecchi';
+            case ('marin-kitagawa'):
+                url = 'https://api.waifu.im/random/?selected_tags=marin-kitagawa&is_nsfw=false&many=true';
                 break;
-            case ('ero'):
-                endpoint = 'nsfw/ero';
+            case ('mori-calliope'):
+                url = 'https://api.waifu.im/random/?selected_tags=mori-calliope&is_nsfw=false&many=true';
                 break;
-            case ('hentai'):
-                endpoint = 'nsfw/hentai';
-                break;
-            case ('nsfwmaid'):
-                endpoint = 'nsfw/maid';
-                break;
-            case ('milf'):
-                endpoint = 'nsfw/milf';
-                break;
-            case ('oppai'):
-                endpoint = 'nsfw/oppai';
-                break;
-            case ('oral'):
-                endpoint = 'nsfw/oral';
-                break;
-            case ('paizuri'):
-                endpoint = 'nsfw/paizuri';
+            case ('raiden-shogun'):
+                url = 'https://api.waifu.im/random/?selected_tags=raiden-shogun&is_nsfw=false&many=true';
                 break;
             case ('selfies'):
-                endpoint = 'nsfw/selfies';
-                break;
-            case ('uniform'):
-                endpoint = 'nsfw/uniform';
+                url = 'https://api.waifu.im/random/?selected_tags=selfies&is_nsfw=false&many=true';
                 break;
             case ('random'):
-                endpoint = 'random';
+                url = 'https://api.waifu.im/random/?is_nsfw=false&many=true';
+                break;
+            // nsfw
+            case ('ass'):
+                url = 'https://api.waifu.im/random/?selected_tags=ass&is_nsfw=true&many=true';
+                break;
+            case ('ecchi'):
+                url = 'https://api.waifu.im/random/?selected_tags=ecchi&is_nsfw=true&many=true';
+                break;
+            case ('ero'):
+                url = 'https://api.waifu.im/random/?selected_tags=ero&is_nsfw=true&many=true';
+                break;
+            case ('hentai'):
+                url = 'https://api.waifu.im/random/?selected_tags=hentai&is_nsfw=true&many=true';
+                break;
+            case ('milf'):
+                url = 'https://api.waifu.im/random/?selected_tags=milf&is_nsfw=true&many=true';
+                break;
+            case ('oral'):
+                url = 'https://api.waifu.im/random/?selected_tags=oral&is_nsfw=true&many=true';
+                break;
+            case ('paizuri'):
+                url = 'https://api.waifu.im/random/?selected_tags=paizuri&is_nsfw=true&many=true';
+                break;
+            case ('breast'):
+                url = 'https://api.waifu.im/random/?selected_tags=breast&is_nsfw=true&many=true';
+                break;
+            case ('nsfwmaid'):
+                url = 'https://api.waifu.im/random/?selected_tags=maid&is_nsfw=true&many=true';
+                break;
+            case ('nsfwuniform'):
+                url = 'https://api.waifu.im/random/?selected_tags=uniform&is_nsfw=true&many=true';
+                break;
+            case ('nsfwwaifu'):
+                url = 'https://api.waifu.im/random/?selected_tags=waifu&is_nsfw=true&many=true';
+                break;
+            case ('nsfwselfies'):
+                url = 'https://api.waifu.im/random/?selected_tags=selfies&is_nsfw=true&many=true';
+                break;
+            case ('nsfwrandom'):
+                url = 'https://api.waifu.im/random/?is_nsfw=true&many=true';
                 break;
         };
-        let result = await fetch(`https://api.waifu.im/${endpoint}?many=true`);
+        let result = await fetch(url);
         result = await result.json();
+        if (result.code === 404) return await msg.reply(JSON.stringify(result));
 
         const images = result.images;
 
