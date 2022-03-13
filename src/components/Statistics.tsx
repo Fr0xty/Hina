e24069835c2f62c77cf566ad7b1410fc4e7c2bf5
Fr0xty <ongjiae8@gmail.com>
2022-03-13 15:22:35 +0800

    fully designed statistics component.

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+import '../scss/Statistics.scss';
+
+import guildIcon from '../images/guild_icon.png';
+import userIcon from '../images/user_icon.png';
+import commandIcon from '../images/command_icon.png';
+
 const Statistics = () => {
+    const [statistics, setStatistics] = useState({
+        guildCount: 'loading..',
+        userCount: 'loading..',
+        commandCount: 'loading..',
+    });
+
+    useEffect(() => {
+        const fetching = async () => {
+            const req = await fetch('/api/hinaweb/statistics');
+            const stats = await req.json();
+
+            setStatistics(stats);
+        };
+        fetching();
+    }, []);
+
     return (
-        <div className="statistics">
-            <div className="guild">Hina is in __ servers</div>
-            <div className="member"></div>
-            <div className="command"></div>
+        <div className="Statistics">
+            <div>
+                <img src={guildIcon} alt="server icon" />
+                <p>
+                    is in <b>{statistics.guildCount}</b> servers!
+                </p>
+            </div>
+            <div className="user-stats">
+                <img src={userIcon} alt="user icon" />
+                <p>
+                    serving approximately <b>{statistics.userCount}</b> unique users!
+                </p>
+            </div>
+            <div>
+                <img src={commandIcon} alt="command icon" />
+                <p>
+                    has <b>{statistics.commandCount}</b> commands!
+                </p>
+            </div>
         </div>
     );
 };
