66e9bc31505d1d0cc2d5f22969d6ea77d97b6fb3
Fr0xty <ongjiae8@gmail.com>
2022-03-15 16:37:20 +0800

    server now renders webapp

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,22 +1,34 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * consts that are not available in ES Module of 'path'.
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * server app instance
  */
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
 /**
  * express routers
  */
 import hinawebRouter from './routes/api/private/hinaweb.js';
-import avatarhistoryRouter from './routes/api/private/fetchavatarhistory.js';
+import fetchavatarhistoryRouter from './routes/api/private/fetchavatarhistory.js';
 app.use('/api/hinaweb', hinawebRouter);
-app.use('/api/fetch-avatar-history', avatarhistoryRouter);
+app.use('api/fetchavatarhistory', fetchavatarhistoryRouter);
 
 /**
- * homepage
+ * render React webapp
  */
-app.get('/', async (req, res) => res.send('Hina is online!'));
+app.get('*', async (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 /**
  * for repl command
