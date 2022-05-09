168ed7f661e471b635a0c38e49158bdc1314d407
Fr0xty <ongjiae8@gmail.com>
2022-05-09 19:31:20 +0800

    added 'anime' command

---
+++ src/commands/fun/anime.ts
@@ -0,0 +1,89 @@
+import fetch from 'node-fetch';
+import { Message, MessageEmbed } from 'discord.js';
+import { loadFile } from 'graphql-import-files';
+
+import { BaseCommand } from 'hina';
+import CommandArgument from '../../res/models/CommandArgument.js';
+import { Hina } from '../../res/config.js';
+
+// TODO: fix avatar not working in webhook
+
+export default class anime implements BaseCommand {
+    name: String;
+    description: String;
+    commandUsage: String;
+    args: CommandArgument[];
+    gqlSchema: String;
+
+    constructor() {
+        this.name = 'anime';
+        this.description = 'get information on animes.';
+        this.commandUsage = '<anime_name>';
+        this.args = [
+            new CommandArgument({ type: 'paragraph' }).setName('anime_name').setDescription('anime to search for.'),
+        ];
+
+        this.gqlSchema = loadFile('./schema/anilistRequestByName.gql');
+    }
+
+    async execute(msg: Message, args: string[]) {
+        const [anime_name] = args;
+
+        /**
+         * fetch info from api
+         */
+        const req = await fetch('https://graphql.anilist.co', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                query: this.gqlSchema,
+                variables: {
+                    name: anime_name,
+                    type: 'ANIME',
+                },
+            }),
+        });
+
+        /**
+         * errors when requesting
+         */
+        if (req.status === 404)
+            return await msg.reply(`Did not find any anime with the search terms: \`${anime_name}\`.`);
+        if (req.status !== 200) {
+            return await msg.reply('Something went wrong with the request. Please try again in a while.');
+        }
+
+        /**
+         * destructure info and replacing null values with dash
+         */
+        let {
+            data: { Media: animeInfo },
+        }: any = JSON.parse(JSON.stringify(await req.json()).replaceAll(':null,', ':"-",'));
+
+        const embed = new MessageEmbed()
+            .setTitle(`${animeInfo.title.native}\n${animeInfo.title.english}`)
+            .setURL(`https://myanimelist.net/anime/${animeInfo.idMal}`)
+            .setDescription(animeInfo.description.replaceAll('<br>', '\n')) // TODO: format all html tags
+            .setColor(Hina.color)
+            .setThumbnail(animeInfo.coverImage[Object.keys(animeInfo.coverImage)[0]])
+            .setFields([
+                { name: 'Format', value: animeInfo.format, inline: true },
+                { name: 'Status', value: animeInfo.status, inline: true },
+                {
+                    name: 'Season',
+                    value: animeInfo.season === '-' ? '-' : `${animeInfo.season} ${animeInfo.seasonYear}`,
+                    inline: true,
+                },
+                { name: 'Episodes', value: String(animeInfo.episodes), inline: true },
+                { name: 'Duration', value: String(animeInfo.duration), inline: true },
+                { name: 'Popularity', value: `#${animeInfo.popularity}`, inline: true },
+                { name: 'Source', value: animeInfo.source, inline: true },
+                { name: 'Genres', value: animeInfo.genres.toString().replaceAll(',', ', '), inline: false },
+            ]);
+
+        await msg.reply({ embeds: [embed] });
+    }
+}
