7465161888a006797b21e6b56f4a6d828dadfd5f
Fr0xty <ongjiae8@gmail.com>
2022-05-20 11:03:06 +0800

    reverted accidental merge

--- dist/events/onReady.js
+++
@@ -1,5 +0,0 @@
-import { Hina } from '../res/config.js';
-Hina.on('ready', async () => {
-    Hina.avatarHistoryChannel = await Hina.channels.fetch('953890455685333032');
-    Hina.owner = await Hina.users.fetch('395587171601350676');
-});
