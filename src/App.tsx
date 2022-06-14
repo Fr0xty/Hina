070e68d8620804b069146f856c50c8fd318a7d8b
Fr0xty <ongjiae8@gmail.com>
2022-06-14 19:46:34 +0800

    renamed command-help to commands

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import AvatarHistory from './pages/AvatarHistory';
 import Home from './pages/Home';
-import CommandHelp from './pages/CommandHelp';
+import Commands from './pages/Commands';
 
 /**
  * pages
  */
-
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/command-help" element={<CommandHelp />} />
+                    <Route path="/commands" element={<Commands />} />
                     <Route path="/api/avatar-history/:userId" element={<AvatarHistory />} />
                 </Routes>
             </Router>
--- src/pages/Commands.tsx
+++ src/pages/Commands.tsx
@@ -1,7 +1,7 @@
-import '../scss/CommandHelp.scss';
+import '../scss/Commands.scss';
 
-const CommandHelp = () => {
+const Commands = () => {
     return <h1>Sorry, this page will be coming (soon?). But I might procrastinate ( 'ω')</h1>;
 };
 
-export default CommandHelp;
+export default Commands;
