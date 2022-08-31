0a256cbcf758d88a8dae958a04a7902b5db72e49
Fr0xty <ongjiae8@gmail.com>
2022-08-31 14:25:01 +0800

    changed hina client intent to enums

--- src/res/hinaSetup/1_initializeHina.ts
+++ src/res/hinaSetup/1_initializeHina.ts
@@ -1,19 +1,19 @@
-import { Client, Intents } from 'discord.js';
+import { Client, IntentsBitField } from 'discord.js';
 
 /**
  * main Hina client instance
  */
 const Hina = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Intents.FLAGS.GUILD_WEBHOOKS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildEmojisAndStickers,
+        IntentsBitField.Flags.GuildWebhooks,
+        IntentsBitField.Flags.GuildInvites,
+        IntentsBitField.Flags.GuildVoiceStates,
+        IntentsBitField.Flags.GuildPresences,
+        IntentsBitField.Flags.GuildMessageReactions,
     ],
 });
 
