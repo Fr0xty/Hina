1cc7ce1e07e4673b89236613f42896004562e280
Fr0xty <ongjiae8@gmail.com>
2022-12-05 18:52:24 +0800

    added debug mode for registering slash commands

--- src/utils/registerSlashCommands.ts
+++ src/utils/registerSlashCommands.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-
 import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 import Hina from '../res/HinaClient.js';
 
@@ -8,12 +7,17 @@ export default async (slashCommands: SlashCommandBuilder[]) => {
     const rest = new REST({ version: '10' }).setToken(Hina.token!);
 
     try {
-        // await rest.put(Routes.applicationCommands(hinaId), {
-        await rest.put(Routes.applicationGuildCommands(hinaId, '859029044942471208'), {
-            body: slashCommands,
-        });
-
-        console.log('Successfully registered application commands.');
+        if (process.env.REGISTER_COMMANDS_DEBUG) {
+            await rest.put(Routes.applicationGuildCommands(hinaId, '859029044942471208'), {
+                body: slashCommands,
+            });
+            console.log('Successfully registered application guild commands.');
+        } else {
+            await rest.put(Routes.applicationCommands(hinaId), {
+                body: slashCommands,
+            });
+            console.log('Successfully registered application global commands.');
+        }
     } catch (err) {
         console.error(err);
     }
