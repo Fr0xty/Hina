a72639faa703079417843a19dd59a377620ec2e5
Fr0xty <ongjiae8@gmail.com>
2022-12-18 14:02:35 +0800

    implemented invite command

---
+++ src/commands/general/invite.ts
@@ -0,0 +1,46 @@
+import {
+    APIActionRowComponent,
+    APIMessageActionRowComponent,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Client,
+    CommandInteraction,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} from 'discord.js';
+import BaseCommand from '../../res/models/BaseCommand.js';
+import { generateHinaInvite } from '../../utils/general.js';
+
+export default class extends BaseCommand {
+    constructor() {
+        super(new SlashCommandBuilder().setName('invite').setDescription('get my invite link.'));
+    }
+
+    async slashExecute(Hina: Client, interaction: CommandInteraction) {
+        const HinaInvite = await generateHinaInvite(Hina);
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: 'My invite link♡', iconURL: Hina.user!.displayAvatarURL(Hina.imageOption) })
+            .setColor(Hina.color)
+            .setDescription(HinaInvite)
+            .setFooter({
+                text: `Requested by: ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL(Hina.imageOption),
+            })
+            .setTimestamp();
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setLabel('Invite Me!')
+                .setStyle(ButtonStyle.Link)
+                .setURL(HinaInvite)
+                .setEmoji('<a:AquaBounce:884003530933944341>')
+        );
+
+        await interaction.reply({
+            embeds: [embed],
+            components: [row],
+        });
+    }
+}
