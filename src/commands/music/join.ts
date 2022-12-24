8bf93b48985792521b64c83f20b44364a0838577
Fr0xty <ongjiae8@gmail.com>
2022-12-24 23:42:30 +0800

    implemented join command

---
+++ src/commands/music/join.ts
@@ -0,0 +1,47 @@
+import { Client, CommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js';
+import BaseCommand from '../../res/models/BaseCommand.js';
+
+export default class extends BaseCommand {
+    constructor() {
+        super(new SlashCommandBuilder().setName('join').setDescription('I will join your voice channel.'));
+    }
+
+    async slashExecute(Hina: Client, interaction: CommandInteraction) {
+        const targetMember = interaction.member as GuildMember;
+
+        /**
+         * target member isn't in a voice channel
+         */
+        if (!targetMember.voice.channel) {
+            return await interaction.reply('Please join a voice channel before using this command.');
+        }
+
+        /**
+         * join
+         */
+        const queue = Hina.player.getQueue(interaction.guild!);
+        if (!queue) {
+            const queue = Hina.player.createQueue(interaction.guild!, {
+                ytdlOptions: {
+                    quality: 'highest',
+                    filter: 'audioonly',
+                    highWaterMark: 1 << 25,
+                    dlChunkSize: 0,
+                },
+                metadata: {
+                    channel: interaction.channel,
+                },
+                leaveOnEnd: false,
+                leaveOnStop: false,
+                leaveOnEmptyCooldown: 180000,
+                initialVolume: 50,
+            });
+            await queue.connect(targetMember.voice.channel);
+        }
+        if (targetMember.voice.channelId !== interaction.guild!.members.me!.voice.channelId) {
+            await queue?.connect(targetMember.voice.channel);
+        }
+
+        await interaction.reply(Hina.okEmoji);
+    }
+}
