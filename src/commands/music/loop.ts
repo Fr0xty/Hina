8ee2a35d75d7f8e1bc5baf9bb000774e952bc0b0
Fr0xty <ongjiae8@gmail.com>
2022-12-25 00:01:02 +0800

    implemented loop command

---
+++ src/commands/music/loop.ts
@@ -0,0 +1,51 @@
+import { Client, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import BaseCommand from '../../res/models/BaseCommand.js';
+
+enum LoopChoices {
+    Cancel,
+    Song,
+    Queue,
+}
+
+export default class extends BaseCommand {
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName('loop')
+                .setDescription('set song loop setting for the song queue in the server.')
+                .addIntegerOption((option) =>
+                    option
+                        .setName('type')
+                        .setDescription('type of looping.')
+                        .setChoices(
+                            { name: 'Cancel', value: LoopChoices.Cancel },
+                            { name: 'Song', value: LoopChoices.Song },
+                            { name: 'Queue', value: LoopChoices.Queue }
+                        )
+                        .setRequired(true)
+                )
+        );
+    }
+
+    async slashExecute(Hina: Client, interaction: CommandInteraction) {
+        const args = {
+            type: interaction.options.get('type')!.value as number,
+        };
+
+        /**
+         * get server song queue
+         */
+        const queue = Hina.player.getQueue(interaction.guild!);
+
+        /**
+         * not playing any song in server
+         */
+        if (!queue) return await interaction.reply("I'm not currently playing in this server.");
+
+        /**
+         * set looping type
+         */
+        queue.setRepeatMode(args.type);
+        await interaction.reply(Hina.okEmoji);
+    }
+}
