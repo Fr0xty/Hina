9571b2d41f832acae00e5e6290283d6de30ce423
Fr0xty <ongjiae8@gmail.com>
2022-12-24 23:46:39 +0800

    implemented leave command

---
+++ src/commands/music/leave.ts
@@ -0,0 +1,23 @@
+import { Client, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import BaseCommand from '../../res/models/BaseCommand.js';
+
+export default class extends BaseCommand {
+    constructor() {
+        super(new SlashCommandBuilder().setName('leave').setDescription('I will leave the voice channel.'));
+    }
+
+    async slashExecute(Hina: Client, interaction: CommandInteraction) {
+        /**
+         * get server song queue
+         */
+        const queue = Hina.player.getQueue(interaction.guild!);
+
+        /**
+         * not in voice channel in the server
+         */
+        if (!queue) return await interaction.reply("I'm not currently playing in this server.");
+
+        queue.destroy(true);
+        await interaction.reply(Hina.okEmoji);
+    }
+}
