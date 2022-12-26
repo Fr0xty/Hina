cf20ac9a4238cc295fc846e2be5bab11a272ed41
Fr0xty <ongjiae8@gmail.com>
2022-12-26 17:22:34 +0800

    implemented pause command

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,23 @@
+import { Client, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import BaseCommand from '../../res/models/BaseCommand.js';
+
+export default class extends BaseCommand {
+    constructor() {
+        super(new SlashCommandBuilder().setName('pause').setDescription('pause current playing song.'));
+    }
+
+    async slashExecute(Hina: Client, interaction: CommandInteraction) {
+        /**
+         * get server song queue
+         */
+        const queue = Hina.player.getQueue(interaction.guild!);
+
+        /**
+         * not playing songs in the server
+         */
+        if (!queue) return await interaction.reply("I'm not currently playing songs in this server.");
+
+        queue.setPaused(true);
+        await interaction.reply(Hina.okEmoji);
+    }
+}
