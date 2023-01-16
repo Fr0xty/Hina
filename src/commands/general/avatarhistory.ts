661adce6d07c1a6b1277c45cf5b16e9474f4b818
Fr0xty <ongjiae8@gmail.com>
2023-01-16 17:45:05 +0800

    implemented avatarhistory command

--- src/commands/general/avatarhistory.ts
+++ src/commands/general/avatarhistory.ts
@@ -15,31 +15,33 @@ export default class extends BaseCommand {
     }
 
     async slashExecute(Hina: Client, interaction: CommandInteraction) {
-        // TODO: reevaluate the code after hinaweb is up again
-        return interaction.reply(
-            'Avatar history is currently inaccessible, but avatars are still being recorded. Sorry for the inconvenience.'
-        );
+        const args = {
+            user: interaction.options.get('user')?.value as string | undefined,
+        };
 
-        // const args = {
-        //     user: interaction.options.get('user')?.value as string | undefined,
-        // };
+        /**
+         * determine user
+         */
+        let targetUser;
+        try {
+            targetUser = args.user ? await Hina.users.fetch(args.user) : interaction.user;
+        } catch {
+            return await interaction.reply('Invalid userId!');
+        }
 
-        // let targetUser;
-        // try {
-        //     targetUser = args.user ? await Hina.users.fetch(args.user) : interaction.user;
-        // } catch {
-        //     return await interaction.reply('Invalid userId!');
-        // }
+        /**
+         * return result
+         */
+        const embed = new EmbedBuilder()
+            .setColor(Hina.color)
+            .setAuthor({ name: `${targetUser.tag}'s Avatar History`, iconURL: targetUser.displayAvatarURL() })
+            .setDescription(`see them [HERE](${process.env.HINAWEB_BASE_URL}/avatar-history/${targetUser.id})`)
+            .setFooter({
+                text: `Requested by ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL(),
+            })
+            .setTimestamp();
 
-        // const embed = new EmbedBuilder()
-        //     .setColor(Hina.color)
-        //     .setAuthor({ name: `${targetUser.tag}'s Avatar History`, iconURL: targetUser.displayAvatarURL() })
-        //     .setDescription(`see them [HERE](${process.env.HINAWEB_BASE_URL}/api/avatar-history/${targetUser.id})`)
-        //     .setFooter({
-        //         text: `Requested by ${interaction.user.tag}`,
-        //         iconURL: interaction.user.displayAvatarURL(),
-        //     })
-        //     .setTimestamp();
-        // await interaction.reply({ embeds: [embed] });
+        await interaction.reply({ embeds: [embed] });
     }
 }
