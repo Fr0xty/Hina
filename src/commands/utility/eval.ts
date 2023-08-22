112da1f60c5b624800a48a522496a204f53706cb
Fr0xty <ongjiae8@gmail.com>
2023-08-22 18:59:36 +0800

    fixed bug & upgraded eval command

--- src/commands/utility/eval.ts
+++ src/commands/utility/eval.ts
@@ -1,4 +1,4 @@
-import { Client, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { Client, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import BaseCommand from '../../res/BaseCommand.js';
 import { extractCodeBlock } from '../../utils/general.js';
 
@@ -30,21 +30,43 @@ export default class extends BaseCommand {
             const msgContent = (await interaction.channel?.messages.fetch(args.message_id))!.content;
             const { code } = await extractCodeBlock(msgContent);
 
-            eval(`
-                (async () => {
-                    const { default: Hina } = await import('../../res/HinaClient.js');
-
-                    try {
-                        ${code?.trim()}
-                    } catch (err) {
-                        return await interaction.reply({ content: err.message, ephemeral: true });
-                    }
-                })()
-            `);
-
             try {
-                await interaction.reply({ content: 'eval ran successfully.', ephemeral: true });
-            } catch {}
+                await eval(`
+                    (async () => {
+                        const { default: Hina } = await import('../../res/HinaClient.js');
+                        
+                        try { ${code?.trim()} }
+                        catch (err) { throw err }
+                    })();
+                `);
+
+                /**
+                 * no error & no reply, send success message
+                 */
+                if (!interaction.replied)
+                    await interaction.reply({ content: 'eval ran successfully.', ephemeral: true });
+            } catch (err) {
+                /**
+                 * error thrown in eval
+                 */
+                if (err instanceof Error) {
+                    const errorEmbed = new EmbedBuilder()
+                        .setColor(Hina.color)
+                        .setTitle('Error')
+                        .setFields([
+                            { name: 'Name', value: '```' + err.name + '```', inline: false },
+                            { name: 'Message', value: '```' + err.message + '```', inline: false },
+                            { name: 'Stack', value: '```' + err.stack + '```' ?? 'none', inline: false },
+                        ]);
+
+                    await interaction.reply({
+                        embeds: [errorEmbed],
+                        ephemeral: true,
+                    });
+                } else {
+                    await interaction.reply({ content: 'Something went wrong.', ephemeral: true });
+                }
+            }
         } catch {
             await interaction.reply({ content: 'No code block was provided in the message.', ephemeral: true });
         }
